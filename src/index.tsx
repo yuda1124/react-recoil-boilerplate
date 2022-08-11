951b91c26bdc144f80448c74b7eae8188d52fad7
yuda1124 <yuda1124@naver.com>
2022-08-12 00:13:33 +0900

    chore: sass 설치 및 reset.scss 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { App } from '@components'
 import { RecoilRoot } from 'recoil'
 import reportWebVitals from './reportWebVitals'
+import '@styles/_reset.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
