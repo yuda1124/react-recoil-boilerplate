590db48b2a58af4af39ee0845323b1ec6a4f5c1c
yuda1124 <yuda1124@naver.com>
2022-08-11 23:51:04 +0900

    feat: recoil 설치 및 기본 설정

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { atom, useRecoilValue } from 'recoil'
 
-function App() {
-  return <div className="App">app</div>
+const appNameAtom = atom({
+  key: 'appNameAtom',
+  default: 'app',
+})
+
+const App = () => {
+  const appName = useRecoilValue(appNameAtom)
+  return <div className="App">{appName}</div>
 }
 
 export { App }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from '@components'
+import { RecoilRoot } from 'recoil'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
 )
 
